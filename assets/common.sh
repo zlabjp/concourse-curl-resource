fdf353f8754fbed9d2797e1e16f90447d4a572e2
Luciano Silva <lsilva@pivotal.io>
2017-02-17 20:39:08 -0500

    Fix version number format issue

--- assets/common.sh
+++ assets/common.sh
@@ -21,14 +21,16 @@ check_version() {
   local httpHeader=$(curl -R -I $1 2>&1 | grep 'Last-Modified:')
   # Checks if field "Last-Modified" exists in HTTP header and transform it into timestamp string
   # if that field is not present, return current timestamp
-  if [ -z "$httpHeader" ]
+  local dateVersionFormat="%Y%m%d%H%S"
+  local dateString=$(date +"$dateVersionFormat")
+
+  if [ ! -z "$httpHeader" ]
   then
-        # echo "Last-Modified information not returned for targeted file. Using current date's timestamp as version number."
-        local dateString=$(date)
-  else
-        # echo "$httpHeader"
-        local dateString=$(echo "$httpHeader" | sed -e "s/Last-Modified: //" | cut -d',' -f 2)
+        # echo "Last-Modified information returned for targeted file. Extract date, removing day of the week string
+        local tmpDateString=$(echo "$httpHeader" | sed -e "s/Last-Modified: //" | cut -d',' -f 2)
+        # rfc1123Format="%d\ %b\ %Y\ %H:%M:%S\ GMT" - in order to work in boot2docker, it has to be outside of quotes in the command below
+        local dateString=$(date +"$dateVersionFormat" -D %d\ %b\ %Y\ %H:%M:%S\ GMT -d "$tmpDateString")
   fi
 
-  date +"%Y%m%d%H%S" -d "$dateString"
+  echo "$dateString"
 }
